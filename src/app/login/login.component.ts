6525c1eb4e341b917c76dc7ab573748ea929c0d3
gmwendt <marcon@elipse.com.br>
2018-05-22 15:37:43 -0300

    current account update [2]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,7 +29,6 @@ export class LoginComponent implements OnInit {
     //DEV MOCK
   /*var user: User = { 
       email: "admin@mrc.com",
-      id: 1,
       name: "admin",
       password: 'ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413',
       userName: "admin"
@@ -38,7 +37,7 @@ export class LoginComponent implements OnInit {
     this._userService.currentUser = user;
     this._userService.typedPassword = '123456';
 
-    this._router.navigate(['home']);  */
+    this._router.navigate(['home']); */ 
   }
 
 
@@ -59,6 +58,7 @@ export class LoginComponent implements OnInit {
 
       this._userService.currentUser = user;
       this._userService.typedPassword = this._pwd;
+      this._account.current = acc[0];
 
       this._router.navigate(['home']); 
 
