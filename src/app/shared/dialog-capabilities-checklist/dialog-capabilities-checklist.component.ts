722d40aa5519a7adfe64f7a293734d669a12c398
Gustavo <gmwendt@hotmail.com>
2018-07-13 06:38:43 -0300

    [start] implementação da dialog capabilities checklist

--- src/app/shared/dialog-capabilities-checklist/dialog-capabilities-checklist.component.ts
+++ src/app/shared/dialog-capabilities-checklist/dialog-capabilities-checklist.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChildren, ViewEncapsulation } from '@angular/core';
 
 import { MatDialogRef } from '../dialog.service';
 
@@ -8,8 +8,19 @@ import { MatDialogRef } from '../dialog.service';
   styleUrls: ['./dialog-capabilities-checklist.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class DialogCapabilitiesChecklistComponent {
-  constructor(private _dialogRef: MatDialogRef<DialogCapabilitiesChecklistComponent>) {
+export class DialogCapabilitiesChecklistComponent implements AfterViewInit {
+
+  @ViewChildren('boxLevel0') _boxLevel0;
+  @ViewChildren('boxLevel1') _boxLevel1;
+
+  constructor(private _dialogRef: MatDialogRef<DialogCapabilitiesChecklistComponent>, private _element: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    console.log(this._boxLevel0);
+    console.log(this._element);
+    this._boxLevel0.last.nativeElement.checked = true;
+    this._boxLevel0.first.nativeElement.indeterminate = true;
   }
 
   private create_clicked(): void {
