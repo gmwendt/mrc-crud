2a95544352df3060fe64a4ecab5646bfba260391
Gustavo <gmwendt@hotmail.com>
2018-09-27 23:24:52 -0300

    [update 270918] dialog professional schedule

--- src/app/shared/time-input/time-input.component.ts
+++ src/app/shared/time-input/time-input.component.ts
@@ -1,18 +1,22 @@
-import { Component, ViewEncapsulation, Input } from "@angular/core";
+import { Component, ViewEncapsulation, Input, } from "@angular/core";
 
 @Component({
   selector: 'time-input',
   templateUrl: './time-input.component.html',
   //styleUrls: ['./time-input.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class TimeInputComponent {
 
   @Input()
-  value: string;
+  value: string = "";
 
   private on_key_press(e: KeyboardEvent): void {
-    if (e.charCode < 48 || e.charCode > 57)
+    if ((e.charCode < 48 || e.charCode > 58 ) && e.keyCode != 9)
       e.preventDefault();
+  }
+
+  private on_blur(): void {
+    console.log('on_blur');
   }
 }
