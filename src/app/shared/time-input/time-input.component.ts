493999aaa3a96df42a8f299cab8efa5f1999b1dc
Gustavo <gmwendt@hotmail.com>
2018-10-09 23:20:52 -0300

    [update 091018] dialog professional schedule

--- src/app/shared/time-input/time-input.component.ts
+++ src/app/shared/time-input/time-input.component.ts
@@ -3,13 +3,16 @@ import { Component, ElementRef, Input, ViewChild, ViewEncapsulation, } from "@an
 @Component({
   selector: 'time-input',
   templateUrl: './time-input.component.html',
-  //styleUrls: ['./time-input.component.css'],
+  styleUrls: ['./time-input.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
 export class TimeInputComponent {
 
   @Input()
   value: string = "";
+
+  private _maxNumbers: number = 4;
+  private _maxCharacters: number = 5;
 
   @ViewChild('box') box: ElementRef;
 
@@ -19,11 +22,20 @@ export class TimeInputComponent {
     
     var key = String.fromCharCode(e.which || e.charCode || e.keyCode);
     if (/([0-9])/.test(key)) {
-        var text = this.box.nativeElement.value;
-        var caret = this.caret;
-        var output = text.substring(0, caret);
-        this.box.nativeElement.value = output + key + text.substring(caret + 1);
-        this.box.nativeElement.setSelectionRange(caret + 1, caret + 1);//setCaretPosition(this, caret + 1);
+        var text: string = this.box.nativeElement.value;
+        var textNumbers = text.replace(/[^0-9]/g,"").length;
+
+        if ((textNumbers == this._maxNumbers && this.caret == this._maxNumbers) ||
+            (text.length == this._maxCharacters && this.caret == this._maxCharacters)) {
+          e.preventDefault();
+          return;
+        }
+
+        var output = text.substring(0, this.caret);
+
+        this.box.nativeElement.value = output + key + text.substring(this.caret + 1);
+        this.box.nativeElement.setSelectionRange(this.caret + 1, this.caret + 1);
+
         e.preventDefault();
     }
   }
