476a25b9c38433518ec3a89ce3cacb0d8880e166
Marcon <marcon@elipse.com.br>
2018-10-11 17:06:10 -0300

    [update 111018] dialog professional schedule

--- src/app/shared/time-input/time-input.component.ts
+++ src/app/shared/time-input/time-input.component.ts
@@ -11,37 +11,61 @@ export class TimeInputComponent {
   @Input()
   value: string = "";
 
-  private _maxNumbers: number = 4;
-  private _maxCharacters: number = 5;
+  private _maxCharacters: number = 2;
+  private _minValue: number = 0
 
-  @ViewChild('box') box: ElementRef;
+  private on_key_down(e: KeyboardEvent, target: HTMLInputElement, max: number): void {
+    //UP ARROW
+    if (e.keyCode == 38) { 
+      var value: number = this.isNullOrEmpty(target.value) ? 0 : parseInt(target.value);
+      value++;
 
-  private on_key_press(e: KeyboardEvent): void {
-    if ((e.charCode < 48 || e.charCode > 58 ) && e.keyCode != 9 && e.keyCode != 8)
+      if (value > max)
+        value = max;
+
+      target.value = value.toString();
+    }
+    //DOWN ARROW
+    else if (e.keyCode == 40) { 
+      var value: number = this.isNullOrEmpty(target.value) ? 0 : parseInt(target.value);
+      value--;
+
+      if (value < this._minValue)
+        value = this._minValue;
+
+      target.value = value.toString();
+    }
+  }
+
+  private on_key_press(e: KeyboardEvent, target: HTMLInputElement): void {
+    if ((e.charCode < 48 || e.charCode > 58 ) && e.keyCode != 9 && e.keyCode != 8 && !e.ctrlKey)
       e.preventDefault();
     
     var key = String.fromCharCode(e.which || e.charCode || e.keyCode);
     if (/([0-9])/.test(key)) {
-        var text: string = this.box.nativeElement.value;
-        var textNumbers = text.replace(/[^0-9]/g,"").length;
-
-        if ((textNumbers == this._maxNumbers && this.caret == this._maxNumbers) ||
-            (text.length == this._maxCharacters && this.caret == this._maxCharacters)) {
+        var text: string = target.value;
+        
+        if (text.length == this._maxCharacters && this.caret(target) == this._maxCharacters) {
           e.preventDefault();
           return;
         }
 
-        var output = text.substring(0, this.caret);
+        var caret = this.caret(target);
+        var output = text.substring(0, caret);
 
-        this.box.nativeElement.value = output + key + text.substring(this.caret + 1);
-        this.box.nativeElement.setSelectionRange(this.caret + 1, this.caret + 1);
+        target.value = output + key + text.substring(caret + 1);
+        target.setSelectionRange(caret + 1, caret + 1);
 
         e.preventDefault();
     }
   }
 
-  private get caret(): number {
-    return this.box.nativeElement.selectionStart;
+  private caret(input: HTMLInputElement): number {
+    return input.selectionStart;
+  }
+
+  private isNullOrEmpty(content: string): boolean {
+    return content == null || content.length < 1;
   }
 
   private on_blur(): void {
